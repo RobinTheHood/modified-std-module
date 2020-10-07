61cf80c136542f0082a00ae5d4c35c7cb26b9684
Robin Wieschendorf <mail@robinwieschendorf.de>
2020-10-07 16:47:18 +0200

    feat: add new class Configuration

---
+++ new_files/vendor-no-composer/robinthehood/ModifiedStdModule/Classes/Configuration.php
@@ -0,0 +1,65 @@
+<?php
+namespace RobinTheHood\ModifiedStdModule\Classes;
+
+class Configuration
+{
+    private $prefix;
+
+    public function __construct($prefix)
+    {
+        $this->prefix = $prefix;
+
+        $constants = $this->filterConstants($prefix);
+        
+        $this->defineVariablesFromContants($constants, $prefix);
+    }
+
+    private function filterConstants($prefix)
+    {
+        $result = [];
+
+        $constants = get_defined_constants();
+        foreach ($constants as $key => $constantValue) {
+            if (\strpos($key, $prefix) !== 0) {
+                continue;
+            }
+
+            $result[$key] = $constantValue;
+        }
+
+        return $result;
+    }
+
+    private function defineVariablesFromContants($constants, $prefix)
+    {
+        foreach ($constants as $key => $value) {
+            $var = $this->removePrefix($key, $prefix);
+            $var = $this->screamingCaseToCamelCase($var);
+            
+            $this->$var = $value;
+        }
+    }
+
+    private function removePrefix($string, $prefix)
+    {
+        return str_replace($prefix, '', $string);
+    }
+
+    private function screamingCaseToCamelCase($string)
+    {
+        $parts = explode('_', $string);
+
+        foreach ($parts as &$part) {
+            if (!$part) {
+                continue;
+            }
+
+            $part = strtolower($part);
+            $part = ucfirst($part);
+        }
+
+        $string = implode('', $parts);
+        $string = lcfirst($string);
+        return $string;
+    }
+}
