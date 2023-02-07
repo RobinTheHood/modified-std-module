8179f94127eb1aa391e3ac10928d9e2bbb2e805e
grandeljay <github.jay@grandel.anonaddy.me>
2023-02-07 13:14:53 +0100

    feat: introduce case converter
    The case converter contains static methods which perform various case conversions. In order to avoid breaking changes, the existing methods are left with a deprecated notice and hint towards the new case converter methods.

---
+++ new_files/vendor-no-composer/robinthehood/ModifiedStdModule/Classes/CaseConverter.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace RobinTheHood\ModifiedStdModule\Classes;
+
+class CaseConverter
+{
+    /**
+     * Convert SCREAMING_SNAKE_CASE to camelCase.
+     *
+     * @param string $screamingString The SCREAMING_SNAKE_CASE to convert.
+     *
+     * @return string
+     */
+    public static function screamingToCamel(string $screamingString): string
+    {
+        $screamingParts = explode('_', $screamingString);
+        $camelParts = [];
+        $camelString = '';
+
+        foreach ($screamingParts as $screamingPart) {
+            $camelPart = $screamingPart;
+            $camelPart = strtolower($camelPart);
+            $camelPart = ucfirst($camelPart);
+
+            $camelParts[] = $camelPart;
+        }
+
+        $camelString = implode('', $camelParts);
+        $camelString = lcfirst($string);
+
+        return $camelString;
+    }
+
+    /**
+     * Convert SCREAMING_SNAKE_CASE to lisp-case.
+     *
+     * @param string $screamingString The SCREAMING_SNAKE_CASE to convert.
+     *
+     * @return string
+     */
+    public static function screamingToLisp(string $screamingString): string
+    {
+        $screamingParts = explode('_', $screamingString);
+        $lispParts = [];
+        $lispString = '';
+
+        foreach ($screamingParts as $screamingPart) {
+            $lispPart = strtolower($screamingPart);
+
+            $lispParts[] = $lispPart;
+        }
+
+        $lispString = implode('-', $lispParts);
+
+        return $lispString;
+    }
+}
