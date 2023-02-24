78ae66e094c14e312dd7f8013589009570613edf
grandeljay <github.jay@grandel.anonaddy.me>
2023-02-24 12:50:27 +0100

    test: add mock data

--- tests/StdModuleTest.php
+++ tests/StdModuleTest.php
@@ -7,17 +7,41 @@ use RobinTheHood\ModifiedStdModule\Classes\StdModule;
 
 final class StdModuleTest extends TestCase
 {
-    private static array $moduleNames = [
-        'MODULE_SYSTEM_MC_MY_FIRST_MODULE_WHICH_DOESNT_EXIST',
-        'MODULE_EXPORT_MC_MY_FIRST_MODULE_WHICH_DOESNT_EXIST',
-        'MODULE_SHIPPING_MCMYFIRSTMODULE_WHICH_DOESNT_EXIST',
-        'MODULE_PAYMENT_MC_MY_FIRST_MODULE_WHICH_DOESNT_EXIST',
-        'MODULE_ORDER_TOTAL_MC_MY_FIRST_MODULE_WHICH_DOESNT_EXIST',
+    private static array $moduleNamesActive = [
+        'MODULE_SYSTEM_MC_MY_FIRST_ACTIVE_MODULE',
+        'MODULE_EXPORT_MC_MY_FIRST_ACTIVE_MODULE',
+        'MODULE_SHIPPING_MCMYFIRSTACTIVEMODULE',
+        'MODULE_PAYMENT_MC_MY_FIRST_ACTIVE_MODULE',
+        'MODULE_ORDER_TOTAL_MC_MY_FIRST_ACTIVE_MODULE',
     ];
+    private static array $moduleNamesInactive = [
+        'MODULE_SYSTEM_MC_MY_FIRST_INACTIVE_MODULE',
+        'MODULE_EXPORT_MC_MY_FIRST_INACTIVE_MODULE',
+        'MODULE_SHIPPING_MCMYFIRSTINACTIVEMODULE',
+        'MODULE_PAYMENT_MC_MY_FIRST_INACTIVE_MODULE',
+        'MODULE_ORDER_TOTAL_MC_MY_FIRST_INACTIVE_MODULE',
+    ];
+
+    protected function setUp(): void
+    {
+        foreach (self::$moduleNamesActive as $moduleName) {
+            define($moduleName . '_STATUS', 'true');
+        }
+
+        foreach (self::$moduleNamesInactive as $moduleName) {
+            define($moduleName . '_STATUS', 'false');
+        }
+    }
 
     public function testIsEnabled(): void
     {
-        foreach (self::$moduleNames as $moduleName) {
+        foreach (self::$moduleNamesActive as $moduleName) {
+            $isEnabled = StdModule::isEnabled($moduleName);
+
+            $this->assertTrue($isEnabled);
+        }
+
+        foreach (self::$moduleNamesInactive as $moduleName) {
             $isEnabled = StdModule::isEnabled($moduleName);
 
             $this->assertFalse($isEnabled);
@@ -26,7 +50,13 @@ final class StdModuleTest extends TestCase
 
     public function testIsDisabled(): void
     {
-        foreach (self::$moduleNames as $moduleName) {
+        foreach (self::$moduleNamesActive as $moduleName) {
+            $isDisabled = StdModule::isDisabled($moduleName);
+
+            $this->assertFalse($isDisabled);
+        }
+
+        foreach (self::$moduleNamesInactive as $moduleName) {
             $isDisabled = StdModule::isDisabled($moduleName);
 
             $this->assertTrue($isDisabled);
