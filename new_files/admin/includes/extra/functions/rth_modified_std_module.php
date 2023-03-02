804b55e96685eb39f77c243f4f89e555c38ee8af
Robin Wieschendorf <mail@robinwieschendorf.de>
2023-03-02 09:44:45 +0100

    feat: support for new mmlc feature vendor-mmlc

--- new_files/admin/includes/extra/functions/rth_modified_std_module.php
+++ new_files/admin/includes/extra/functions/rth_modified_std_module.php
@@ -1,3 +1,5 @@
 <?php
 
+declare(strict_types=1);
+
 require_once DIR_FS_DOCUMENT_ROOT . '/includes/extra/functions/rth_modified_std_module.php';
--- new_files/includes/extra/functions/rth_modified_std_module.php
+++ new_files/includes/extra/functions/rth_modified_std_module.php
@@ -1,12 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 use RobinTheHood\ModifiedStdModule\Classes\StdModule;
 
 // Normaly the autload.php is loaded by the composer/autoload, but we do not know
 // the file order in /includes/extra/functions/. So if this file is loaded first,
 // the composer/autoload dose not loaded the autoload.php already and we have to
 // to load autoload.php by ourself.
-require_once DIR_FS_DOCUMENT_ROOT . '/vendor-no-composer/autoload.php';
+if (file_exists(DIR_FS_DOCUMENT_ROOT . '/vendor-mmlc/autoload.php')) {
+    require_once DIR_FS_DOCUMENT_ROOT . '/vendor-mmlc/autoload.php';
+} else {
+    require_once DIR_FS_DOCUMENT_ROOT . '/vendor-no-composer/autoload.php';
+}
 
 if (!function_exists('rth_is_module_enabled')) {
     function rth_is_module_enabled(string $moduleName): bool
