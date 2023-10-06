1ee6b4e22260b09e997845171301f9961e6c716d
Robin Wieschendorf <mail@robinwieschendorf.de>
2023-10-06 22:08:36 +0200

    chore: commit files

---
+++ new_files/rth_modified_std_module.php
@@ -0,0 +1,8 @@
+<?php
+
+use RobinTheHood\ModifiedStdModule\Classes\Test;
+
+require 'includes/application_top.php';
+
+$test = new Test();
+$test->test();
---
+++ src-mmlc/Classes/Test.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace RobinTheHood\ModifiedStdModule\Classes;
+
+class Test
+{
+    public function test()
+    {
+        die('Test 123');
+    }
+}
+
+ddd
